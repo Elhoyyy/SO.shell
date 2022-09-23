9ffec27073e6ce35d228533a573a5c731f03b31a
danipemos <100313413+danipemos@users.noreply.github.com>
2022-09-23 15:33:43 +0200

    Update list.c

--- list.c
+++ list.c
@@ -1,11 +1,10 @@
-//
-// Created by danip on 22/09/2022.
-//
 
-#include "list.h"
+#include "headed_linked_list.h"
 void createList(tList* L){
-    *L=LNULL;
-    head=*L ;
+    tPosL cabeceira;
+    cabeceira=malloc(sizeof(**L));
+    *L=cabeceira;
+    cabeceira->next=NULL;
 }
 
 bool createNode( tPosL* p){
@@ -15,47 +14,30 @@ bool createNode( tPosL* p){
 
 
 
-char getChar( tPosL p, tList L){
-    return p->datos; //RETORNA LvOS DATOS DE LA POSICIÓN QUE SE INTRODUCE.
+char* getChar( tPosL p, tList L){
+    return &p->datos; //RETORNA LvOS DATOS DE LA POSICIÓN QUE SE INTRODUCE.
 }
 
 void printList(tList L){
     tPosL p;
-    for(p=L;p->next!=NULL;p=p->next){
-        printf("%c",getChar(p,L));
+    for(p=L->next;p->next!=NULL;p=p->next){
+        printf("%s",getChar(p,L));
     }
 }
 
-tPosL findPosition(tList L, char* d) {
-    tPosL p;
-    for (p = L; (p != NULL) && strcmp(&p->datos, d)<0; p = p->next);
-    return p;
 
-}
-
-bool insertItem(char* d, tList *L) {
+bool insertItem(char *d, tList L) {
     tPosL p, q, r; //LAS VARIABLES 'r' Y 'q' ACTUÁN COMO NODOS PARA APUNTAR AL NODO QUE QUEREMOS INTRODUCIR EN LA LISTA.
     if (!createNode(&q)) { //SI NO HAY NODO SE RETORNA FALSO.
         return false;
     } else {
         q->datos = *d;
         q->next = LNULL;
-        if (*L == LNULL) { //LISTA VACÍA.
-            *L = q;
-        } else if (strcmp(*d, &((*L)->datos))<0) { //INSERTAR EN EL FINAL.
-            q->next = *L;
-            *L = q;
-        } else {
-            p = findPosition(*L, d);
-            if (p == LNULL) {
-                for (r = *L; r->next != LNULL; r = r->next);
-                r->next = q;
-            } else {
-                q->datos = p->datos;
-                p->datos = *d;
-                q->next = p->next;
-                p->next = q;
-            }
+        if (L->next == LNULL) { //LISTA VACÍA.
+            L->next = q;
+        } else{
+            for(p=L->next;p->next!=NULL;p=p->next);
+            p->next=q;
         }
         return true;
     }
@@ -68,15 +50,13 @@ void printn(char* param[],tList L){
     p=L;
     for(int i=0;i<n;i++){
         p=p->next;
-    printf("%c", getChar(p,L));
-       }
+    printf("%s", getChar(p,L));
+    }
 }
 
 void deleteAtPosition (tPosL p, tList *L) {
     tPosL q;
-    if (p == *L) { //ELIMINAR ELEMENTO DEL INICIO.
-        *L = (*L)->next;
-    } else if (p->next == LNULL) { //ELIMINAR ELEMENTO DEL FINAL.
+     if (p->next == LNULL) { //ELIMINAR ELEMENTO DEL FINAL.
         for (q = *L; q->next != p; q = q->next);
         q->next = LNULL;
 
