ad71d105fab8acc702f1350caaec4ba1401d8a15
Elhoyyy <100317972+Elhoyyy@users.noreply.github.com>
2022-09-24 16:34:52 +0200

    Update list.h

--- list.h
+++ list.h
@@ -23,4 +23,4 @@ char* getChar(tPosL p,tList L);
 void printList(tList L);
 bool insertItem(char d,tList L);
 void printn(char* param[],tList L);
-void deleteAtPosition (tP
+void deleteAtPosition (tPosL p, tList *L);
