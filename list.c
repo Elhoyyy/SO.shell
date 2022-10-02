d03408f7dea2d364d850aed6d96ac430c362b590
Elhoyyy <100317972+Elhoyyy@users.noreply.github.com>
2022-10-02 13:32:58 +0200

    Update list.c

--- list.c
+++ list.c
@@ -46,15 +46,16 @@ bool insertItem(char d, tList L) {
 }
 
 
+
 void printn(char* param[],tList L){
     tPosL p;
     int counter=0;
     for(p=L->next;p->next!=NULL;p=p->next){
         counter++;
     }
-    long n= strtol(*param,LNULL,10); // teoricamente transforma el char en un int lo busque en internet
+    long n= strtol(param[1],LNULL,10); // teoricamente transforma el char en un int lo busque en internet
     p=L->next;
-    if(n>counter) {
+    if(n<=counter) {
         for (int i = 0; i < n; i++) {
             p = p->next;
             printf("%s", getChar(p, L));
