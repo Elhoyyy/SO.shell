1856ce873e30d5f167b5da4db659823cdfd51488
danipemos <100313413+danipemos@users.noreply.github.com>
2022-11-03 18:19:45 +0100

    Update headed_linked_memorylist.c

--- headed_linked_memorylist.c
+++ headed_linked_memorylist.c
@@ -1,95 +1,96 @@
 
 
-#include <sys/stat.h>
+//
+// Created by dani on 30/10/22.
+//
 #include "headed_linked_memorylist.h"
-#include "headed_linked_list.h"
+#include <sys/stat.h>
+#include <time.h>
 
-void InsertarNodoMmap(tList ListaMemoria, int tipo, void * p, long int st_size, char * fichero){
-    char fecha[50];
-    if(tipo==1){
-        struct malloc_t *mallc= malloc(sizeof (struct malloc_t));
-        mallc->tamano=st_size;
-        mallc->address=p;
-        time_t actual;
-        strftime(fecha, 30, "%a %b %d %T %Y", localtime (&actual));
-        insertItem((char *) mallc, ListaMemoria);
-
-    }else if(tipo==2){
-        struct mmap_t* mmap= malloc((sizeof (struct mmap_t)));
-        mmap->tamano=st_size;
-        mmap->address=p;
-        strcpy(mmap->name,fichero);
-        time_t actual;
-        strftime(fecha, 30, "%a %b %d %T %Y", localtime (&actual));
-        insertItem((char *) mmap, ListaMemoria);
-
-    }
-    else {
-        struct shared_t *shared= malloc(sizeof (struct shared_t));
-        shared->address=p;
-        shared->tamano=st_size;
-        time_t actual;
-        strftime(fecha, 30, "%a %b %d %T %Y", localtime (&actual));
-        insertItem((char *) shared, ListaMemoria);
-    }
+void createMemoryList(MemoryList * L){
+    pos cabeceira;
+    cabeceira=malloc(sizeof(**L));
+    *L=cabeceira;
+    cabeceira->next=NULL;
 }
 
-void *getAdrress(tList L,tPosL p){
-    if(p==NULL){
-        return NULL;
-    }
-    else{
-        return p->datos;
-    }
+bool createNodeMem( pos* p){
+    *p= malloc(sizeof(**p));
+    return *p!=LNULL;
 }
 
+bool insertMemory(MemoryList L, int tipo, char* dirrecion,time_t t,long tamano, int df, char* nombre) {
+    pos p, q; //LAS VARIABLES 'r' Y 'q' ACTUÁN COMO NODOS PARA APUNTAR AL NODO QUE QUEREMOS INTRODUCIR EN LA LISTA.
+    if (!createNodeMem(&q)) { //SI NO HAY NODO SE RETORNA FALSO.
+        return false;
+    } else {
+        if(tipo==1||tipo==3){
+        q->address=dirrecion;
+        q->size=tamano;
+        q->tipo=tipo;
+        q->time=t;
+        q->next = LNULL;
+        }else{
+            q->address=dirrecion;
+            q->size=tamano;
+            q->tipo=tipo;
+            q->time=t;
+            q->next = LNULL;
+            q->id=df;
+            strcpy(q->fich,nombre);
+        }
+        if (L->next == LNULL) { //LISTA VACÍA.
+            L->next = q;
+        } else{
+            for(p=L->next;p->next!=NULL;p=p->next);
+            p->next=q;
+        }
+        return true;
+    }
+}
+char *ptr2string( const void *ptr ) {
+    int size = snprintf( NULL, 0, "%p", ptr );
+    char *result = calloc( size + 1, 1 );
+    sprintf( result, "%p", ptr );
+    return result;
+}
 
-void deleteAtPosition (tPosL p, tList* L) {
-    tPosL q;
-    q = (*L)->next;
-    if (p == (*L)->next) { //ELIMINAR ELEMENTO DEL INICIO.
-        (*L)->next = (*L)->next;
+pos EncontrarPosicion(MemoryList Lista, char* direccion) {
+    pos p;
+    for (p = Lista->next; (p != NULL) && strcmp(ptr2string(p->address), direccion)<0; p = p->next);
+    return p;
+
+}
+
+pos EncontrarTamano(MemoryList Lista, long tamano) {
+    pos p;
+    for (p = Lista->next; (p != NULL) && p->size!=tamano; p = p->next);
+    return p;
+
+}
+void deleteAtPosition (pos p, MemoryList L) {
+    pos q;
+    if (p == L->next) { //ELIMINAR ELEMENTO DEL INICIO.
+        L = L->next->next;
     } else if (p->next == LNULL) { //ELIMINAR ELEMENTO DEL FINAL.
-        for (q = (*L)->next; q->next != p; q = q->next);
+        for (q = L; q->next != p; q = q->next);
         q->next = LNULL;
 
     } else { //ELIMINAR ELEMENTO DEL MEDIO.
         q = p->next;
-        strcpy((p->datos), q->datos);
+        strcpy(p->parms,p->parms);
+        strcpy(p->fich,p->fich);
+        p->address=q->address;
+        p->size=q->size;
+        p->tipo=q->tipo;
+        p->time=q->time;
+        p->id=q->id;
         p->next = q->next;
         p = q;
     }
-    free(p->datos);
     free(p);
 
 }
-
-void printListaMememoria(tList L, int tipo){
-    tPosL p;
-
-    if(tipo == 1) {
-        for (p = L->next; p->next != NULL; p = p->next) {
-            struct malloc_t *malloc = getAdrress(L->next, p);
-            //PONER EL TIEMPO
-            printf("\t%p %d %s ", malloc->address, malloc->tamano, malloc->time);
-            printf("malloc\n");
-        }
-    }
-    if(tipo == 2) {
-        for (p = L->next; p->next != NULL; p = p->next) {
-
-            struct mmap_t *mmap = getAdrress(L->next, p);
-            //TIEMPO
-            printf("\t%p %d %s ", mmap->address, mmap->tamano, mmap->time);
-            printf("mmap %s (fd:%d)\n", mmap->name, mmap->id);
-        }
-    }
-    if(tipo == 3) {
-        for (p = L->next; p->next != NULL; p = p->next) {
-            struct shared_t *shared = getAdrress(L->next, p);
-            //TIEMPO
-            printf("\t%p %d %s ", shared->address, shared->tamano, shared->time);
-            printf("shared memory with key: %d\n",shared->key);
-        }
-    }
+char* getAdrres(pos p){
+     return p->address;
 }
