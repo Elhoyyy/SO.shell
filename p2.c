bd935ad4444164c35f8518d31bdf5c88d179fce6
danipemos <100313413+danipemos@users.noreply.github.com>
2022-12-04 12:42:03 +0100

    Update p2.c

--- p2.c
+++ p2.c
@@ -33,79 +33,113 @@ DATE: 20/10/2022
 #define MMAP 3
 #define N 48
 #define TAMANO 2048
-/*
-static struct SEN sigstrnum[]={
-        {"HUP", SIGHUP},
-        {"INT", SIGINT},
-        {"QUIT", SIGQUIT},
-        {"ILL", SIGILL},
-        {"TRAP", SIGTRAP},
-        {"ABRT", SIGABRT},
-        {"IOT", SIGIOT},
-        {"BUS", SIGBUS},
-        {"FPE", SIGFPE},
-        {"KILL", SIGKILL},
-        {"USR1", SIGUSR1},
-        {"SEGV", SIGSEGV},
-        {"USR2", SIGUSR2},
-        {"PIPE", SIGPIPE},
-        {"ALRM", SIGALRM},
-        {"TERM", SIGTERM},
-        {"CHLD", SIGCHLD},
-        {"CONT", SIGCONT},
-        {"STOP", SIGSTOP},
-        {"TSTP", SIGTSTP},
-        {"TTIN", SIGTTIN},
-        {"TTOU", SIGTTOU},
-        {"URG", SIGURG},
-        {"XCPU", SIGXCPU},
-        {"XFSZ", SIGXFSZ},
-        {"VTALRM", SIGVTALRM},
-        {"PROF", SIGPROF},
-        {"WINCH", SIGWINCH},
-        {"IO", SIGIO},
-        {"SYS", SIGSYS},
 
+/*************************************************************/
 
-#ifdef SIGPOLL
-        {"POLL", SIGPOLL},
-#endif
-#ifdef SIGPWR
-        {"PWR", SIGPWR},
-#endif
-#ifdef SIGEMT
-        {"EMT", SIGEMT},
-#endif
-#ifdef SIGINFO
-        {"INFO", SIGINFO},
-#endif
-#ifdef SIGSTKFLT
-        {"STKFLT", SIGSTKFLT},
-#endif
-#ifdef SIGCLD
-        {"CLD", SIGCLD},
-#endif
-#ifdef SIGLOST
-        {"LOST", SIGLOST},
-#endif
-#ifdef SIGCANCEL
-        {"CANCEL", SIGCANCEL},
-#endif
-#ifdef SIGTHAW
-        {"THAW", SIGTHAW},
-#endif
-#ifdef SIGFREEZE
-        {"FREEZE", SIGFREEZE},
-#endif
-#ifdef SIGLWP
-        {"LWP", SIGLWP},
-#endif
-#ifdef SIGWAITING
-        {"WAITING", SIGWAITING},
-#endif
-        {NULL,-1},
+/*las siguientes funciones nos permiten obtener el nombre de una senal a partir
+del número y viceversa */
+#define MAXVAR 2048
+#define MAXNAME 2048
+struct SEN {
+	char * nombre;
+	int  senal;
 };
-*/
+
+static struct SEN sigstrnum[]={   
+	{"HUP", SIGHUP},
+	{"INT", SIGINT},
+	{"QUIT", SIGQUIT},
+	{"ILL", SIGILL}, 
+	{"TRAP", SIGTRAP},
+	{"ABRT", SIGABRT},
+	{"IOT", SIGIOT},
+	{"BUS", SIGBUS},
+	{"FPE", SIGFPE},
+	{"KILL", SIGKILL},
+	{"USR1", SIGUSR1},
+	{"SEGV", SIGSEGV},
+	{"USR2", SIGUSR2}, 
+	{"PIPE", SIGPIPE},
+	{"ALRM", SIGALRM},
+	{"TERM", SIGTERM},
+	{"CHLD", SIGCHLD},
+	{"CONT", SIGCONT},
+	{"STOP", SIGSTOP},
+	{"TSTP", SIGTSTP}, 
+	{"TTIN", SIGTTIN},
+	{"TTOU", SIGTTOU},
+	{"URG", SIGURG},
+	{"XCPU", SIGXCPU},
+	{"XFSZ", SIGXFSZ},
+	{"VTALRM", SIGVTALRM},
+	{"PROF", SIGPROF},
+	{"WINCH", SIGWINCH}, 
+	{"IO", SIGIO},
+	{"SYS", SIGSYS},
+/*senales que no hay en todas partes*/
+            #ifdef SIGPOLL
+	{"POLL", SIGPOLL},
+            #endif
+            #ifdef SIGPWR
+	{"PWR", SIGPWR},
+            #endif
+            #ifdef SIGEMT
+	{"EMT", SIGEMT},
+            #endif
+            #ifdef SIGINFO
+	{"INFO", SIGINFO},
+            #endif
+            #ifdef SIGSTKFLT
+	{"STKFLT", SIGSTKFLT},
+            #endif
+            #ifdef SIGCLD
+	{"CLD", SIGCLD},
+            #endif
+            #ifdef SIGLOST
+	{"LOST", SIGLOST},
+            #endif
+            #ifdef SIGCANCEL
+	{"CANCEL", SIGCANCEL},
+            #endif
+            #ifdef SIGTHAW
+	{"THAW", SIGTHAW},
+            #endif
+            #ifdef SIGFREEZE
+	{"FREEZE", SIGFREEZE},
+            #endif
+            #ifdef SIGLWP
+	{"LWP", SIGLWP},
+            #endif
+            #ifdef SIGWAITING
+	{"WAITING", SIGWAITING},
+            #endif
+ 	{NULL,-1},
+	};    /*fin array sigstrnum */
+
+
+int ValorSenal(char * sen)  /*devuelve el numero de senial a partir del nombre*/ 
+{ 
+  int i;
+  for (i=0; sigstrnum[i].nombre!=NULL; i++)
+  	if (!strcmp(sen, sigstrnum[i].nombre))
+		return sigstrnum[i].senal;
+  return -1;
+}
+
+
+char *NombreSenal(int sen)  /*devuelve el nombre senal a partir de la senal*/ 
+{			/* para sitios donde no hay sig2str*/
+ int i;
+  for (i=0; sigstrnum[i].nombre!=NULL; i++)
+  	if (sen==sigstrnum[i].senal)
+		return sigstrnum[i].nombre;
+ return ("SIGUNKNOWN");
+}
+
+
+
+
+/********************************************/
 
 void leerEntrada(char *cadena);
 int TrocearCadena(char *cadena, char *trozos[]);
@@ -1631,5 +1665,3 @@ char *NombreSenal(int sen)
  return ("SIGUNKNOWN");
 }
  */
-
-
